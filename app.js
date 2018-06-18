1c05731b131928c090379de894a917ebc6fd50a2
Erkin KUNBUK <35873324+SxQDeV@users.noreply.github.com>
2018-06-18 09:51:04 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-// const http = require('http');
-// const express = require('express');
-// const app = express();
-// app.get('/', (request, response) => {
-//   console.log(Date.now() + ' Ping Received');
-//   response.sendStatus(200);
-// });
-// app.listen(process.env.PORT);
-// setInterval(() => {
-//   http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-// }, 200000);
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get('/', (request, response) => {
+  console.log(Date.now() + ' Ping Received');
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 200000);
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
